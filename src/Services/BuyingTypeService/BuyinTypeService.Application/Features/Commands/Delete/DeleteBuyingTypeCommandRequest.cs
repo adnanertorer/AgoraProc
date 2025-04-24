9b4857f76687be804918b998e07681b0c3bfd4d4
adnanertorer <adnanertorer@hotmail.com>
2025-04-25 01:12:24 +0300

    added DeleteBuyingTypeCommandRequestValidator

--- src/Services/BuyingTypeService/BuyinTypeService.Application/Features/Commands/Delete/DeleteBuyingTypeCommandRequest.cs
+++ src/Services/BuyingTypeService/BuyinTypeService.Application/Features/Commands/Delete/DeleteBuyingTypeCommandRequest.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BuyingTypeService.Application.Abstracts;
 using BuyingTypeService.Application.Dtos;
 using BuyingTypeService.Application.Wrappers;
 using MediatR;
@@ -11,4 +7,13 @@ namespace BuyingTypeService.Application.Features.Commands.Delete;
 
 public class DeleteBuyingTypeCommandRequest : IRequest<ResponseResult<BuyingTypeModel>>
 {
+    public long Id { get; set; }
+
+    public class DeleteBuyingTypeCommandRequestHandler(IBuyingTypeRepository buyingTypeRepository) : IRequestHandler<DeleteBuyingTypeCommandRequest, ResponseResult<BuyingTypeModel>>
+    {
+        public Task<ResponseResult<BuyingTypeModel>> Handle(DeleteBuyingTypeCommandRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
