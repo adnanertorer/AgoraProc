c60f02d8b0e9c38ab158285a953cd2cad2bac335
adnanertorer <adnanertorer@hotmail.com>
2025-04-25 03:31:19 +0300

    added service collections

---
+++ src/Services/BuyingTypeService/BuyinTypeService.Application/BuyingTypeServiceCollection.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using Adoroid.Core.Application.Rules;
+using BuyingTypeService.Application.Features.Commands.Create;
+using BuyingTypeService.Application.Features.Commands.Create.Validators;
+using BuyingTypeService.Application.Features.Commands.Update;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MinimalMediatR.Extensions;
+using FluentValidation;
+
+namespace BuyingTypeService.Application;
+
+public static class BuyingTypeServiceCollection
+{
+    public static IServiceCollection AddBuyingTypeServiceCollection(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSubClassesOfType(Assembly.GetExecutingAssembly(), type: typeof(BaseBusinessRule));
+        services.AddMinimalMediatR(typeof(CreateBuyingTypeCommandRequest).Assembly, typeof(UpdateBuyingTypeCommandRequest).Assembly, typeof(CreateBuyingTypeCommandRequestValidator).Assembly);
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        return services;
+    }
+
+    private static IServiceCollection AddSubClassesOfType(this IServiceCollection services, Assembly assembly, Type type,
+    Func<IServiceCollection, Type, IServiceCollection>? addWithLifeCycle = null)
+    {
+        var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+        foreach (var item in types)
+            if (addWithLifeCycle == null)
+                services.AddScoped(item);
+            else
+                addWithLifeCycle(services, type);
+        return services;
+    }
+}
---
+++ src/Services/BuyingTypeService/BuyingTypeService.Persistence/BuyinTypePersistenceServiceCollection.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using BuyingTypeService.Persistence.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuyingTypeService.Persistence;
+
+public static class BuyinTypePersistenceServiceCollection
+{
+    public static IServiceCollection AddBuyinTypePersistenceServiceCollection(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<BuyingTypeDbContext>(options =>
+        {
+            var connectionStrings = configuration.GetSection(ConnectionStringOption.Key).Get<ConnectionStringOption>();
+            options.UseNpgsql(connectionStrings!.DefaultConnection, m => m.MigrationsAssembly(Assembly.GetExecutingAssembly()));
+        });
+
+        return services;
+    }
+}
