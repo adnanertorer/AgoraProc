c2aa0b5afc4fcb4d0fb86dd1982798ba06c287dd
adnanertorer <adnanertorer@hotmail.com>
2025-04-29 00:13:27 +0300

    added migator, some refactoring

--- src/Services/BuyingTypeService/BuyinTypeService.Application/Features/Commands/Delete/DeleteBuyingTypeCommandRequest.cs
+++ src/Services/BuyingTypeService/BuyinTypeService.Application/Features/Commands/Delete/DeleteBuyingTypeCommandRequest.cs
@@ -10,24 +10,25 @@ namespace BuyingTypeService.Application.Features.Commands.Delete;
 public class DeleteBuyingTypeCommandRequest : IRequest<ResponseResult<BuyingTypeModel>>
 {
     public long Id { get; set; }
+}
 
-    public class DeleteBuyingTypeCommandRequestHandler(IBuyingTypeRepository buyingTypeRepository) : IRequestHandler<DeleteBuyingTypeCommandRequest, ResponseResult<BuyingTypeModel>>
+public class DeleteBuyingTypeCommandRequestHandler(IBuyingTypeRepository buyingTypeRepository) : IRequestHandler<DeleteBuyingTypeCommandRequest, ResponseResult<BuyingTypeModel>>
+{
+    public async Task<ResponseResult<BuyingTypeModel>> Handle(DeleteBuyingTypeCommandRequest request, CancellationToken cancellationToken)
     {
-        public async Task<ResponseResult<BuyingTypeModel>> Handle(DeleteBuyingTypeCommandRequest request, CancellationToken cancellationToken)
+        var model = await buyingTypeRepository.GetAsync(predicate: i => i.Id == request.Id, cancellationToken: cancellationToken);
+        if (model == null)
         {
-            var model = await buyingTypeRepository.GetAsync(predicate: i => i.Id == request.Id, cancellationToken: cancellationToken);
-            if (model == null) {
-                return ResponseResult<BuyingTypeModel>.Fail(BusinessMessages.BuyingTypeNotFound);
-            }
-
-            model.DeletedDate = DateTime.UtcNow;
-            model.DeletedBy = "system";
-            model.IsDeleted = true;
-
-            var entity = await buyingTypeRepository.UpdateAsync(model);
-
-            return ResponseResult<BuyingTypeModel>.Success(new BuyingTypeModel 
-            { Id = model.Id, BuyingTypeName = model.BuyingTypeName, TenantId = model.TenantId },InfoMessages.BuyingTypeDeletedSuccessfully);
+            return ResponseResult<BuyingTypeModel>.Fail(BusinessMessages.BuyingTypeNotFound);
         }
+
+        model.DeletedDate = DateTime.UtcNow;
+        model.DeletedBy = "system";
+        model.IsDeleted = true;
+
+        var entity = await buyingTypeRepository.UpdateAsync(model);
+
+        return ResponseResult<BuyingTypeModel>.Success(new BuyingTypeModel
+        { Id = model.Id, BuyingTypeName = model.BuyingTypeName, TenantId = model.TenantId }, InfoMessages.BuyingTypeDeletedSuccessfully);
     }
 }
--- src/Services/BuyingTypeService/BuyinTypeService.Application/Features/Queries/GetById/GetBuyingTypeByIdRequest.cs
+++ src/Services/BuyingTypeService/BuyinTypeService.Application/Features/Queries/GetById/GetBuyingTypeByIdRequest.cs
@@ -9,19 +9,19 @@ namespace BuyingTypeService.Application.Features.Queries.GetById;
 public class GetBuyingTypeByIdRequest : IRequest<ResponseResult<BuyingTypeModel>>
 {
     public long Id { get; set; }
+}
 
-    public class GetBuyingTypeByIdRequestHandler(IBuyingTypeRepository buyingTypeRepository) : IRequestHandler<GetBuyingTypeByIdRequest, ResponseResult<BuyingTypeModel>>
+public class GetBuyingTypeByIdRequestHandler(IBuyingTypeRepository buyingTypeRepository) : IRequestHandler<GetBuyingTypeByIdRequest, ResponseResult<BuyingTypeModel>>
+{
+    public async Task<ResponseResult<BuyingTypeModel>> Handle(GetBuyingTypeByIdRequest request, CancellationToken cancellationToken)
     {
-        public async Task<ResponseResult<BuyingTypeModel>> Handle(GetBuyingTypeByIdRequest request, CancellationToken cancellationToken)
+        var model = await buyingTypeRepository.GetAsync(predicate: i => i.Id == request.Id, cancellationToken: cancellationToken);
+        if (model == null)
         {
-            var model = await buyingTypeRepository.GetAsync(predicate: i => i.Id == request.Id, cancellationToken: cancellationToken);
-            if (model == null)
-            {
-                return ResponseResult<BuyingTypeModel>.Fail(BusinessMessages.BuyingTypeNotFound);
-            }
-
-            return ResponseResult<BuyingTypeModel>.Success(new BuyingTypeModel
-            { Id = model.Id, BuyingTypeName = model.BuyingTypeName, TenantId = model.TenantId });
+            return ResponseResult<BuyingTypeModel>.Fail(BusinessMessages.BuyingTypeNotFound);
         }
+
+        return ResponseResult<BuyingTypeModel>.Success(new BuyingTypeModel
+        { Id = model.Id, BuyingTypeName = model.BuyingTypeName, TenantId = model.TenantId });
     }
 }
